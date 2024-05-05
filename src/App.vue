42363aa65082959170e7a1099ce11f7028916ab7
Rick <tao993859833@live.cn>
2024-05-05 16:47:44 +0800

    初始化提交

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,26 @@
-<!--
- * @Description: APP
- * @Author: 安知鱼
- * @Email: anzhiyu-c@qq.com
- * @Date: 2022-10-31 13:27:51
- * @LastEditTime: 2022-11-30 08:51:24
- * @LastEditors: 安知鱼
--->
-<script setup lang="ts">
-import MainTop from "./components/main-top.vue";
-import MainContent from "./components/main-content.vue";
-import Controller from "./components/controller.vue";
-import Tip from "./components/tip.vue";
-import Copyright from "./components/copyright.vue";
-</script>
-
 <template>
-  <main class="air-conditioner-vue">
-    <MainTop />
-    <Tip />
-    <MainContent />
-    <Controller />
-    <Copyright />
-  </main>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
-<style scoped lang="scss">
-.air-conditioner-vue {
-  display: flex;
-  flex-direction: column;
-  flex: 1 auto;
-  margin: 0 auto;
-  width: 100%;
-  max-width: 600px;
-  padding: 1.5rem 10px 10px;
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
